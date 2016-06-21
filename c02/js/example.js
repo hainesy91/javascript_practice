389cc4f6530db98b334e9daaf147782c4b28d8cf
Chris <hainesy91@gmail.com>
2016-06-21 15:58:40 +0100

    created welcome message variables and concatenated them together in a greeting variable

---
+++ c02/js/example.js
@@ -0,0 +1,9 @@
+//Create variables for the welcome message
+var greeting = 'Howdy ';
+var name = 'Chris';
+var message = '. Please check your order:';
+
+//Concatenate the three variables above to create the welcome message
+var welcome = greeting + name + message;
+
+//Create
