f83288d9c47d7be7e71b1f4cc5ffd933ceecba42
Chris <hainesy91@gmail.com>
2016-06-23 15:22:32 +0100

    created variables for the hotel name, room rate and special rate

--- c03/js/example.js
+++ c03/js/example.js
@@ -15,4 +15,16 @@
       return offerRate;
     } 
   };
+
+  //Write out the Hotel name, standard rate, and the special rate
+
+  var hotelName, roomRate, specialRate; //Declare Variables
+
+  hotelName = document.getElementById('hotelName'); //Get Elements
+  roomRate = document.getElementById('roomRate');
+  specialRate = document.getElementById('specialRate');
+
+  hotelName.textContent = hotel.name; //Write the Hotel Name
+  roomRate.textContent = '$' + hotel.roomRate.toFixed(2); //Write room rate
+  specialRate.textContent = '$' + hotel.offerPrice(); //Write Offer price
 })
