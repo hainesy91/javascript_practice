64a54ad98afe4f1bdcc20d16a8568a156e847262
Chris <hainesy91@gmail.com>
2016-06-23 15:27:50 +0100

    wrote function that calculates the expiry dates and offers for the hotel

--- c03/js/example.js
+++ c03/js/example.js
@@ -27,4 +27,25 @@
   hotelName.textContent = hotel.name; //Write the Hotel Name
   roomRate.textContent = '$' + hotel.roomRate.toFixed(2); //Write room rate
   specialRate.textContent = '$' + hotel.offerPrice(); //Write Offer price
+
+  //Part Two: Calculate and Write out the expiry details for the offer
+
+  var expiryMsg; //Message displayed to users
+  var today; //Today's date
+  var elEnds; //The element that shows the message about the offer ending
+
+  function offerExpires(today) {
+    //Declare variables within the function for local scope
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+    //Add 7 days time (added in milliseconds)
+    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+    //Create arrays to hold the names of the days/months
+  }
+
+
+
+
+
+
+
 })
