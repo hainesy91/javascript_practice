fd402091d74c37926671b7b303d58c2fcf4f7aa2
Chris <hainesy91@gmail.com>
2016-06-28 16:42:11 +0100

    wrote code that checks if the user passed both rounds, store the result in a variable

--- c04/logical-and.js
+++ c04/logical-and.js
@@ -4,3 +4,8 @@ var score1 = 8; //Round 1 score
 var score2 = 8; //Round 2 score 
 var pass1 = 6; //Round 1 pass mark
 var pass2 = 6; //Round 2 pass mark
+
+//Check whether the user passed both rounds, store result in a variable
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
+
+//
