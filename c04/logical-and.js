12eb9e6ebd4d9577bcb955a08033e8b3e54292f8
Chris <hainesy91@gmail.com>
2016-06-28 16:40:46 +0100

    added score variables

--- c04/logical-and.js
+++ c04/logical-and.js
@@ -1,2 +1,6 @@
 //The logical AND is used to see if the user's score is greater than ot equal to the pass mark in both rounds of the test. The result is stored in a variable called passBoth
 
+var score1 = 8; //Round 1 score
+var score2 = 8; //Round 2 score 
+var pass1 = 6; //Round 1 pass mark
+var pass2 = 6; //Round 2 pass mark
