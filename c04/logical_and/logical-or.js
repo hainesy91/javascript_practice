48ef9a9ec5587f18ada8812f82bc72e1fde245f6
Chris <hainesy91@gmail.com>
2016-06-28 16:52:38 +0100

    declared the instructions and defined the score variables again

---
+++ c04/logical_and/logical-or.js
@@ -0,0 +1,6 @@
+//Here is the same test but this time using the logical OR operator to find out if the user has passed at least one of the two rounds. If they pass just one round they do not need to retake the test.
+
+var score1 = 8; //Round 1 score
+var score2 = 8; //Round 2 score 
+var pass1 = 6; //Round 1 pass mark
+var pass2 = 6; //Round 2 pass mark
