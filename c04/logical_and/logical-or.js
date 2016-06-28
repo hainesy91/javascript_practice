e7f8de1d24510071dcf05053a765786d76730000
Chris <hainesy91@gmail.com>
2016-06-28 16:54:15 +0100

    checked whether the user passed on of the two rounds using the logical or operator

--- c04/logical_and/logical-or.js
+++ c04/logical_and/logical-or.js
@@ -4,3 +4,6 @@ var score1 = 8; //Round 1 score
 var score2 = 8; //Round 2 score 
 var pass1 = 6; //Round 1 pass mark
 var pass2 = 6; //Round 2 pass mark
+
+//Check whether the user passed one of the two rounds, store the result in a variable
+var minPass = ((score1 >= pass1) || (score2 >= pass2));
