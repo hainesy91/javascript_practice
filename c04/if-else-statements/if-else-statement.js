bf782678ef001311394a4b6bf875758ef51acd09
Chris <hainesy91@gmail.com>
2016-06-28 22:32:59 +0100

    stated simple if else proceedure and defined starting variables

--- c04/if-else-statements/if-else-statement.js
+++ c04/if-else-statements/if-else-statement.js
@@ -1,1 +1,7 @@
-console.log('Hello Chris');
+//Here you can see that an if...else statement allows you to provide two sets of code.
+//1. One set if the condition evaluates to true
+//2. Another set if the condition is false
+
+var pass = 50; //Pass Mark
+var score = 75; //Current Score
+var msg; //Message
