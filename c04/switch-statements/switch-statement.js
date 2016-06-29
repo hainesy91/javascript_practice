b75cd03411c120d0ffb5a8c88e07f93301e00d75
Chris <hainesy91@gmail.com>
2016-06-29 11:28:28 +0100

    wrote instructions explaining purpose of the script and defined the starting variables of the switch statement

--- c04/switch-statements/switch-statement.js
+++ c04/switch-statements/switch-statement.js
@@ -1,1 +1,4 @@
-console.log('Hello Chris');
+//In this example, the purpose of the switch statement is to present the user with a different message depending on which level they are at. The message is stored in a variable called msg.
+
+var msg; //Message
+var level = 2; //Level
