f7f9b51dd56181e5c2321c38660a208042d42f7e
Chris <hainesy91@gmail.com>
2016-06-29 11:37:43 +0100

    Finished example by explaining the process of the switch statements

--- c04/switch-statements/switch-statement.js
+++ c04/switch-statements/switch-statement.js
@@ -24,3 +24,9 @@ switch (level) {
 
 var el = document.getElementById('answer');
 el.textContent = msg;
+
+//The variable called level contains a number indicating which level the user is on. This is then used as the switch value.(The switch value could also be an expression).
+//In the following code block, there are 3 options for what the value of the level variable might be: the numbers: 1, 2 or 3.
+//If the value of the level variable is the number 1, the value of the message variable is set to 'Good luck on the first test'. And so on for cases 2 and 3.
+//If no match is found, then the value of the msg variable is set to 'Good Luck!'
+//Each case ends with the break keyword which will tell the JavaScript interpreter to skip the rest of this code and continute onto the next.
