331fa959c309891f604b1ba054fc022bbebc8bd8
Chris <hainesy91@gmail.com>
2016-06-29 11:31:53 +0100

    wrote the switch statement with the 3 cases and the default option

--- c04/switch-statements/switch-statement.js
+++ c04/switch-statements/switch-statement.js
@@ -2,3 +2,22 @@
 
 var msg; //Message
 var level = 2; //Level
+
+//Determine message based on the level
+switch (level) {
+  case 1:
+    msg = 'Good luck on the first test';
+    break;
+
+  case 2:
+    msg = 'Second of three - keep going!';
+    break;
+
+  case 3:
+    msg = 'Final round, almost there!';
+    break;
+
+  default:
+  msg = 'Good Luck!';
+  break;
+}
