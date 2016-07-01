338bd08d1bee55360bbc02aaed3e913faf04d9fd
Chris <hainesy91@gmail.com>
2016-07-01 15:09:14 +0100

    wrote instructions explaining the purpose of the example

--- c04/loops/while-loop.js
+++ c04/loops/while-loop.js
@@ -1,1 +1,3 @@
-console.log('Rihanna is great');
+//Here is an example of a while loop. It writes out the 5 times table. Each time the loop is run, another calculation is written into the variable called msg.
+
+//This loop will continue to run for as long as the condition in the parenthesis is true. That condition is a counter indicating that, as long as the variable i remains less than 10, the statements in the subsequent code block should run.
