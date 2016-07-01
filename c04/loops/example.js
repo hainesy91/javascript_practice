1c8a26783a40ac9a9ff251e3e8102d3aa08e978c
Chris <hainesy91@gmail.com>
2016-07-01 14:48:07 +0100

    added starting variables for the loop

--- c04/loops/example.js
+++ c04/loops/example.js
@@ -5,3 +5,9 @@ for (var i = 0; i < 10; i++) {
 }
 
 //A for loop is often used to loop through items in an array. In this example, the scores for each round of a test are stored in an array called scores.
+
+var scores = [24, 32, 17]; //Array of scores
+var arrayLength = scores.length; //Items in array
+var roundNumber = 0; //Current Round
+var msg = ''; //Message
+var 1; //Counter
