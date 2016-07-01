be70b4382f3565b8c1a340dc64db707c23cf418f
Chris <hainesy91@gmail.com>
2016-07-01 14:55:57 +0100

    write code that writes the results of the array numbers to the screen, and cleared up some missing data

--- c04/loops/example.js
+++ c04/loops/example.js
@@ -1,19 +1,15 @@
 console.log('Hello Chris');
 
-for (var i = 0; i < 10; i++) {
-     document.write(i);
-}
-
 //A for loop is often used to loop through items in an array. In this example, the scores for each round of a test are stored in an array called scores.
 
 var scores = [24, 32, 17]; //Array of scores
 var arrayLength = scores.length; //Items in array
 var roundNumber = 0; //Current Round
 var msg = ''; //Message
-var 1; //Counter
+var i; //Counter
 
 //Loop through the items in the array
-for (i = 0 < arrayLength; i++) {
+for (i = 0; i < arrayLength; i++) {
   //Arrays are zero based (so 0 is round 1)
   //Add 1 to the current round
   roundNumber = (i + 1);
@@ -22,3 +18,5 @@ for (i = 0 < arrayLength; i++) {
   //Get the score from the scores array
   msg += scores[i] + '<br />';
 }
+
+document.getElementById('answer').innerHTML = msg;
