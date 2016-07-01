68cfb42a4e98b9d49e9a243b7c66a32804fe9b82
Chris <hainesy91@gmail.com>
2016-07-01 15:59:30 +0100

    wrote if else and while loop

--- c04/decisions-loops/example.js
+++ c04/decisions-loops/example.js
@@ -2,3 +2,15 @@ var table = 3;  //Unit of table
 var operator = 'addition'; //Type of calculation (defaults to addition)
 var i = 1; //Set counter to 1
 var msg = ''; //Message
+
+if (operator === 'addition') { //If the operator variable says addtion
+  while (i < 11) { //While counter is less than 11
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />'; //Calculation
+    i++; //Add one to the counter
+  }
+} else { //Otherwise
+  while (i < 11) { //While counter is less than 11
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />'; //Calculation
+    i++; //Add one to the counter
+  }
+}
