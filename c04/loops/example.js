91e19ec1c6ab306fff8496a77498a192c5e9587d
Chris <hainesy91@gmail.com>
2016-07-01 14:46:28 +0100

    wrote explanation of loop example

--- c04/loops/example.js
+++ c04/loops/example.js
@@ -1,1 +1,7 @@
 console.log('Hello Chris');
+
+for (var i = 0; i < 10; i++) {
+     document.write(i);
+}
+
+//A for loop is often used to loop through items in an array. In this example, the scores for each round of a test are stored in an array called scores.
