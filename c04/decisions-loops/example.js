91cd9c647ee487ea08d39628b8b59ef0bd24a90e
Chris <hainesy91@gmail.com>
2016-07-01 16:05:01 +0100

    added fina explanation of the code to the pgae

--- c04/decisions-loops/example.js
+++ c04/decisions-loops/example.js
@@ -18,3 +18,9 @@ if (operator === 'addition') { //If the operator variable says addtion
 //Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg; 
+
+//The if statement checks whether the value of the variable called operator is addition. If it is, it uses a while loop to perform the calculations and stores the result in a variable called msg.
+
+//If you change the value of the operator variable to anything other than addition, the conditional statement will selecte set of statements. These also contain a while loop, but this time it will perform multiplication rather than addition.
+
+//When one of the loops has finished running, the last two lines of the script select the element whose id attribute has a value of blackboard, and updates the page within the content of the msg variable.
