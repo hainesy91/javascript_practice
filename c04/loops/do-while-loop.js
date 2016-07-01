59ba28c8c556dd13a7233d00a73a0f87229fe73a
Chris <hainesy91@gmail.com>
2016-07-01 15:29:33 +0100

    set up starting variables for the do while loop

--- c04/loops/do-while-loop.js
+++ c04/loops/do-while-loop.js
@@ -1,1 +1,4 @@
 //The key difference between a while loop and a do while loop is that the statements in the code block come before the condition. This means that those statements are run once whether or not the condition is met.
+
+var i = 1 //Set counter to 1
+var msg = ''; //Message
