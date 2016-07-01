cd4890097b2b780a1598cad535b14bb2b5cce52f
Chris <hainesy91@gmail.com>
2016-07-01 15:09:55 +0100

    declared starting variables

--- c04/loops/while-loop.js
+++ c04/loops/while-loop.js
@@ -1,3 +1,6 @@
 //Here is an example of a while loop. It writes out the 5 times table. Each time the loop is run, another calculation is written into the variable called msg.
 
 //This loop will continue to run for as long as the condition in the parenthesis is true. That condition is a counter indicating that, as long as the variable i remains less than 10, the statements in the subsequent code block should run.
+
+var i = 1; //Set counter to 1
+var msg = ''; //Message
