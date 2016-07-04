bd45a516e6cc4260e03194ab5378bae6e53f7ec5
Chris <hainesy91@gmail.com>
2016-07-04 13:45:14 +0200

    created a text node and stored it in a variable with js

--- c05/example.js
+++ c05/example.js
@@ -3,3 +3,9 @@ console.log('Hello World');
 var el = document.getElementById('shoppingList');
 
 var one = document.getElementById('one').textContent;
+
+//Create a new element and store it in a variable
+var newEl = document.createElement('li');
+
+//Create a text node and store it in a variable
+var newText = document.createTextNode('quinoa');
