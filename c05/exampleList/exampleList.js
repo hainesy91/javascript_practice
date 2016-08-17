dbee3bce310c268e1bb2c81c6f0ca1213db1a323
Chris <hainesy91@gmail.com>
2016-08-17 10:55:50 +0100

    wrote javascript code that got the ul element and created a new element with a text node, added it to the element and then added it to the bottom of the list

---
+++ c05/exampleList/exampleList.js
@@ -0,0 +1,8 @@
+//Adding Items to Start And End of List
+var list = document.getElementsByTagName('ul')[0]; //Get the ul element
+
+//Add New Item to end of List
+var newItemLast = document.createElement('li'); //Create element
+var newTextLast = document.createTextNode('cream'); //Create Text node
+newItemLast.appendChild(newTextLast); //Add Text Node to Element
+list.appendChild(newItemLast); //Add Element to end of List
