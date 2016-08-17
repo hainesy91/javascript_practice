93e97ad6bc49c5fc3f17540667ed20282aa1e8ae
Chris <hainesy91@gmail.com>
2016-08-17 11:09:39 +0100

    wrote js that created an element and text node, added the text node to the element and then added it to the list at the start

--- c05/exampleList/exampleList.js
+++ c05/exampleList/exampleList.js
@@ -8,3 +8,8 @@ newItemLast.appendChild(newTextLast); //Add Text Node to Element
 list.appendChild(newItemLast); //Add Element to end of List
 
 //Add New Item to Start of List
+var newItemFirst = document.createElement('li'); //Create Element
+var newTextFirst = document.createTextNode('kale'); //Create Text Node
+newItemFirst.appendChild(newTextFirst); //Add text node to element
+list.insertBefore(newItemFirst, list.firstChild); //Add element to the list
+
