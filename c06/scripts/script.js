c87a55e493ccf8df2adee46f17d26832aa9f5542
Chris <hainesy91@gmail.com>
2016-09-08 11:43:08 +0100

    declared variables and stored some elements in the two of the variables

--- c06/scripts/script.js
+++ c06/scripts/script.js
@@ -1,1 +1,6 @@
 console.log('Hello Chris');
+
+var noteInput, noteName, textEntered, target; //Delcare variables
+
+noteName = document.getElementById('noteName'); //Element that holds note
+noteInput = document.getElementById('noteInput'); //Input for writing the note
