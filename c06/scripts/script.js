da4a6581b801707df6dd4f5615cc1f86727ef217
Chris <hainesy91@gmail.com>
2016-09-08 11:47:09 +0100

    wrote function that checks for object and adds ie5-8 fallback if there isnt one, gets the target of the event, the value of that element and updates the note text

--- c06/scripts/script.js
+++ c06/scripts/script.js
@@ -4,3 +4,13 @@ var noteInput, noteName, textEntered, target; //Delcare variables
 
 noteName = document.getElementById('noteName'); //Element that holds note
 noteInput = document.getElementById('noteInput'); //Input for writing the note
+
+function writeLabel(e) {  //Declare function
+  if (!e) {               //If event object is not present
+    e = window.event;     //Use IE5-8 fallback
+  }
+  target = e.target || e.srcElement;  //Get target of event
+  textEntered = target.value;         //Value of that element
+  noteName.textContent = textEntered; //Update note text
+}
+
